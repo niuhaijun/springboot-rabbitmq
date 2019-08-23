7b1effb0923da9caf3fe46c4057484e5cae8eff5
牛海军 <niuhaijun@100tal.com>
2019-08-23 17:20:03 +0800

    备份交换器

---
+++ alternateexchange/src/main/java/com/niu/alternateexchange/AlternateexchangeApplication.java
@@ -0,0 +1,14 @@
+package com.niu.alternateexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternateexchangeApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(AlternateexchangeApplication.class, args);
+  }
+
+}
---
+++ alternateexchange/src/test/java/com/niu/alternateexchange/AlternateexchangeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.niu.alternateexchange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlternateexchangeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+
+  }
+
+}
